cc0347757baa8f0072aa699d4edd1bfa499bdd69
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-02-06 14:13:43 +0530

    11ty and Contentful added

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+const particularsToJson = array => {
+    let newArray = []
+    array.forEach(string => {
+        const splitString = string.split('|')
+        const object = {}
+        object.particular = splitString[0]
+        object.price = splitString[1]
+        newArray.push(object)
+    })
+    return newArray
+}
+
+const subTotal = array =>{
+    let sum = 0
+    array.forEach(string=>{
+        const splitString = string.split('|')
+        const price = splitString[1].trim()
+        sum += parseInt(price)
+    })
+    return sum
+}
+
+module.exports = eleventyConfig => {
+    eleventyConfig.addPassthroughCopy('./src/css')
+    eleventyConfig.addPassthroughCopy('./src/js')
+    eleventyConfig.addPassthroughCopy('./src/img')
+    eleventyConfig.addFilter('particularsToJson', particularsToJson)
+    eleventyConfig.addFilter('subTotal', subTotal)
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+}
